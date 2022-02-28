de91c2820ff6e2d519f9dbfbbea9b9149d123f51
Binod Sharma <binodsharma02331@gmail.com>
2022-02-28 22:25:37 +0545

    modified

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = ({ env }) => ({
   defaultConnection: "default",
+
   connections: {
     default: {
       connector: "mongoose",
@@ -12,39 +13,3 @@ module.exports = ({ env }) => ({
     },
   },
 });
-
-// module.exports = ({ env }) => ({
-//   defaultConnection: "default",
-//   connections: {
-//     default: {
-//       connector: "mongoose",
-//       settings: {
-//         uri: "mongodb+srv://autoregister:subisu980@autoregister.7bhfj.mongodb.net/AutoRegister?retryWrites=true&w=majority",
-//       },
-//       options: {
-//         ssl: true,
-//       },
-//     },
-//   },
-// });
-
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'mongoose',
-//       settings: {
-//         host: env('DATABASE_HOST', '127.0.0.1'),
-//         srv: env.bool('DATABASE_SRV', false),
-//         port: env.int('DATABASE_PORT', 27017),
-//         database: env('DATABASE_NAME', 'auto-register'),
-//         username: env('DATABASE_USERNAME', null),
-//         password: env('DATABASE_PASSWORD', null),
-//       },
-//       options: {
-//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-//         ssl: env.bool('DATABASE_SSL', false),
-//       },
-//     },
-//   },
-// });
